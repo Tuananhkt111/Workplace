5c229bf8a98c422217946d6f947bbcecada1600c
Tuan Anh Hoang <54363990+Tuananhkt111@users.noreply.github.com>
2020-06-04 14:13:14 +0700

    update new demo transoon

---
+++ Mobile/transoon_demo/lib/repositories/user_repository.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class UserRepository {
+  final FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
+
+  UserRepository({FirebaseAuth firebaseAuth, GoogleSignIn googleSignIn})
+    : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
+      _googleSignIn = googleSignIn ?? GoogleSignIn();
+
+  Future<FirebaseUser> signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final AuthCredential authCredential = GoogleAuthProvider
+  }
+}
